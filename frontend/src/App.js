ca916adfe43f3dbb95addbf88a89b95ac03e75d3
Nicolas Valdearenas <nicolas.valdearenas@cloudhesive.com>
2023-06-19 19:03:10 -0300

    bucket and request fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,36 @@ import "./App.css";
 
 const App = () => {
   const [file, setFile] = useState();
+  const [fileContent, setFileContent] = useState();
 
   const handleChange = (event) => {
     setFile(event.target.value);
+    setFileContent(event.target.files[0]);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    axios({
+    const data = await axios({
       method: "post",
       url: "https://ti28z9r6c8.execute-api.us-east-1.amazonaws.com/dev/upload",
       data: {
         filename: file.split("\\").pop(),
       },
+    }).then((res) => res);
+    console.log("data => ", data);
+    console.log("file => ", file);
+    console.log("fileContent => ", fileContent);
+
+    const form = new FormData();
+    Object.keys(data.data.fields).forEach((key) => {
+      form.append(key, data.data.fields[key]);
+    });
+    form.append("file", fileContent);
+
+    await axios({
+      method: "post",
+      url: data.data.url,
+      data: form,
     });
   };
 
